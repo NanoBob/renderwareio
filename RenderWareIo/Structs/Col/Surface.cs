c891fcba31e0796d09c954624a61505352b41eb0
sebajura1234@gmail.com <sebajura1234@gmail.com>
2023-04-01 09:56:13 +0200

    add it

--- RenderWareIo/Structs/Col/Surface.cs
+++ RenderWareIo/Structs/Col/Surface.cs
@@ -11,14 +11,14 @@ namespace RenderWareIo.Structs.Col
     {
         public static int Size => 4;
 
-        public byte Material { get; set; }
+        public MaterialId Material { get; set; }
         public byte Flag { get; set; }
         public byte Brightness { get; set; }
         public byte Light { get; set; }
 
         public Surface Read(Stream stream)
         {
-            this.Material = RenderWareFileHelper.ReadByte(stream);
+            this.Material = (MaterialId)RenderWareFileHelper.ReadByte(stream);
             this.Flag = RenderWareFileHelper.ReadByte(stream);
             this.Brightness = RenderWareFileHelper.ReadByte(stream);
             this.Light = RenderWareFileHelper.ReadByte(stream);
@@ -28,7 +28,7 @@ namespace RenderWareIo.Structs.Col
 
         public void Write(Stream stream)
         {
-            RenderWareFileHelper.WriteByte(stream, this.Material);
+            RenderWareFileHelper.WriteByte(stream, (byte)this.Material);
             RenderWareFileHelper.WriteByte(stream, this.Flag);
             RenderWareFileHelper.WriteByte(stream, this.Brightness);
             RenderWareFileHelper.WriteByte(stream, this.Light);
